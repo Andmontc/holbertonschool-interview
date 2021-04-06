a2a96ec0a4cf3347ad604b123fbb3c86821dd340
Andmontc <oscarandresmontes@gmail.com>
2021-04-06 00:05:55 -0500

    task

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,30 +1,15 @@
 #!/usr/bin/python3
 """
-validate UTF8 module
+Determines if a given data set represents a valid UTF-8 encoding
 """
 
 
 def validUTF8(data):
-    """validUTF8
-    Determines if a given data set represents a valid UTF-8 encoding.
-    Args:
-        data (list): The data set can contain multiple characters
-    Returns:
-        bool: True if data is a valid UTF-8 encoding,
-              else return False.
     """
-    valid = 0
-    for value in data:
-        byte = value & 255
-        if valid:
-            if byte >> 6 != 2:
-                return False
-            valid -= 1
-            continue
-        while (1 << abs(7 - valid)) & byte:
-            valid += 1
-        if valid == 1 or valid > 4:
-            return False
-        valid = max(valid - 1, 0)
-    return valid == 0
-  
+    Return: True if data is a valid UTF-8 encoding, else return False
+    """
+    try:
+        bytes(number & 0xFF for number in data).decode()
+        return True
+    except UnicodeDecodeError:
+        return False
