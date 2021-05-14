27a41e74237afc702dccfb017134f304a26e43fb
Andmontc <oscarandresmontes@gmail.com>
2021-05-14 14:20:23 -0500

    Task files

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -8,36 +8,36 @@
 */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-  skiplist_t *node;
+	skiplist_t *node;
 
-  if (!list)
-    return (NULL);
+	if (!list)
+		return (NULL);
 
-  node = list->express;
-  printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+	node = list->express;
+	printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
 
-  while (node && node->n < value)
-  {
-    list = node;
-    node = node->express;
-    if (!node)
-    {
-      node = list;
-      while (node->next)
-        node = node->next;
-      break;
-    }
-    printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
-  }
-  printf("Value found between indexes [%lu] and [%lu]\n", list->index,
-         node->index);
+	while (node && node->n < value)
+	{
+		list = node;
+		node = node->express;
+		if (!node)
+		{
+			node = list;
+			while (node->next)
+				node = node->next;
+			break;
+		}
+		printf("Value checked at index [%lu] = [%d]\n",	node->index, node->n);
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", list->index,
+			node->index);
 
-  for (; list && node->n >= list->n; list = list->next)
-  {
-    printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+	for (; list && node->n >= list->n; list = list->next)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
 
-    if (list->n == value)
-      return (list);
-  }
-  return (NULL);
+		if (list->n == value)
+			return (list);
+	}
+	return (NULL);
 }
